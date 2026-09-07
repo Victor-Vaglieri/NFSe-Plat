2781d94f33a8a3c678ff7f39abc9da00f72ae339
Victor-Vaglieri <victor_vglr@hotmail.com>
2026-09-07 12:46:51 -0300

    adição da estrutura base do Java

---
+++ backend-java/src/main/java/com/nfse/saas/NfseSaasApplication.java
@@ -0,0 +1,13 @@
+package com.nfse.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfseSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfseSaasApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/nfse/saas/NfseSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfse.saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfseSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
